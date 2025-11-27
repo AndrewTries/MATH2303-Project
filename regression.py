68d2261341a819312b32017b9b6c8bba5708d92e
AndrewTries <akraw904@mtroyal.ca>
2025-11-27 13:58:01 -0700

    cleaned data set

--- regression.py
+++ regression.py
@@ -3,6 +3,12 @@ import pandas as pd
 file_path = "Kidney_Organ_SupplyChain_RawDataset.csv"
 df = pd.read_csv(file_path)
 
+df_clean = df
+
+# binary_columns = ['Donor_Medical_Approval', 'Match_Status', 'Organ_Condition_Alert']
+# for bc in binary_columns:
+#     for i in bc:
+#         df_clean[bc].append(i.replace(i, str(1 if i == 'Yes' else 0)))
 Donor_Medical_Approval = []
 for i in df['Donor_Medical_Approval']:
     Donor_Medical_Approval.append(i.replace(i, str(1 if i == 'Yes' else 0)))
@@ -11,9 +17,9 @@ for i in df['Match_Status']:
     Match_Status.append(i.replace(i, str(1 if i == 'Yes' else 0)))
 Organ_Condition_Alert = []
 for i in df['Organ_Condition_Alert']:
-    Organ_Condition_Alert.append(i.replace(i, str(1 if i == 'Yes' else 0)))
+    Organ_Condition_Alert.append(i.replace(i, str(1 if i == 'Normal' else 0)))
     
-df_clean = df
+
 df_clean['Donor_Medical_Approval'] = Donor_Medical_Approval
 df_clean['Match_Status'] = Match_Status
 df_clean['Organ_Condition_Alert'] = Organ_Condition_Alert
@@ -25,6 +31,8 @@ df_clean.drop(columns=columns_to_drop, inplace=True)
 dummie_columns = ['Patient_BloodType', 'Donor_BloodType', 'Diagnosis_Result', 'Organ_Status']
 df_clean = pd.get_dummies(df_clean, columns=dummie_columns, dtype=int)
 print(df_clean.head)
+
+df_clean.to_csv("../Final Project/cleaned_data.csv", index=False)
     
     
 # print(df['Patient_ID', 'Patient_Age', 'Patient_Weight', 'Patient_BMI',
